100e214c18d9e2118e0a335bea45f4a33f85d55e
gokdefterim <mustafaakalfa34@gmail.com>
2022-08-11 01:13:41 +0300

    next generation bölümünü tamamen değiştirdim, header, body ve footer'da pixel ayarlamaları yaptım, footer-social ikonlarına javascript ile hover verdim - daha kısa bir yol bulunmalı

---
+++ script.js
@@ -0,0 +1,39 @@
+greenFacebook = function(){
+    document.getElementById('facebook').src='images/icon-facebook-2.svg'
+};
+
+whiteFacebook = function(){
+    document.getElementById('facebook').src='images/icon-facebook.svg'
+};
+
+greenYoutube = function(){
+    document.getElementById('youtube').src='images/icon-youtube-2.svg'
+};
+
+whiteYoutube = function(){
+    document.getElementById('youtube').src='images/icon-youtube.svg'
+};
+
+greenTwitter = function(){
+    document.getElementById('twitter').src='images/icon-twitter-2.svg'
+};
+
+whiteTwitter = function(){
+    document.getElementById('twitter').src='images/icon-twitter.svg'
+};
+
+greenPinterest = function(){
+    document.getElementById('pinterest').src='images/icon-pinterest-2.svg'
+};
+
+whitePinterest = function(){
+    document.getElementById('pinterest').src='images/icon-pinterest.svg'
+};
+
+greenInstagram = function(){
+    document.getElementById('instagram').src='images/icon-instagram-2.svg'
+};
+
+whiteInstagram = function(){
+    document.getElementById('instagram').src='images/icon-instagram.svg'
+};
